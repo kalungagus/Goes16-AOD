6e3154648b93aa6baee2d0dbe63f89f4fe5b234b
gustavo <gustavo@datatraffic.com.br>
2020-03-03 22:54:29 -0300

    Adicionado suporte a arquivos de tamanho de partículas e de presença de fumaça

--- analiseQt.py
+++ analiseQt.py
@@ -66,121 +66,141 @@ class MyApp(UIClass, QtBaseClass):
                 # AODF - Aerosol Optical Depth: 'AOD'
                 if product == "AODF":
                     variable = 'AOD'
+                    qvariable = 'DQF'
                     nomenclature = 'AEROPT'
-                    # Close the NetCDF file after getting the data
+                # CPSF - Cloud Particle Size: 'PSD'
+                elif product == "CPSF":
+                    variable = 'PSD'
+                    qvariable = 'DQF'
+                    nomenclature = 'CLDPAS'
+                # ADPF - Aerosol Detection: 'Smoke'
+                elif product == "ADPF":
+                    variable = 'Smoke'
+                    qvariable = 'DQF'
+                    nomenclature = 'AERDET'
+                    #variable = 'Dust'
+                else:
                     nc.close()
-                    # Call the reprojection funcion
-                    grid = remap(path, variable, extent, resolution, x1, y1, x2, y2)
-                    quality = remap(path, 'DQF', extent, resolution, x1, y1, x2, y2)
-                    # Read the data returned by the function 
-                    data = grid.ReadAsArray()
-                    data2 = quality.ReadAsArray()
-                    # Convert from int16 to uint16
-                    data = data.astype(np.float64)
-                    data[data == max(data[0])] = np.nan
-                    data[data == min(data[0])] = np.nan
-                    data2 = data2.astype(np.float64)
-                    data2[data2 == max(data2[0])] = np.nan
-                    data2[data2 == min(data2[0])] = np.nan
-                    # Usar um backend não iterativo para que a imagem não fique aparecendo
-                    # durante o processamento
-                    plt.switch_backend('Agg')
-                    # Define o tamanho da figura salva
-                    plt.figure(figsize=(2000/float(DPI), 2000/float(DPI)), frameon=True, dpi=DPI)
-                    # Plota os dados =======================================================================================
-                    # Cria a referência basemap para a projeção retangular
-                    bmap = Basemap(llcrnrlon=extent[0], llcrnrlat=extent[1], urcrnrlon=extent[2], urcrnrlat=extent[3], epsg=4326)
-                     
-                    # Desenha os países e os estados brasileiros
-                    bmap.readshapefile('Shapefiles\\BRA_adm1','BRA_adm1',linewidth=0.50,color='cyan')
-                    bmap.readshapefile('Shapefiles\\ne_10m_admin_0_countries','ne_10m_admin_0_countries',linewidth=0.50,color='cyan')
+                    continue
+                # Close the NetCDF file after getting the data
+                nc.close()
+                # Call the reprojection funcion
+                grid = remap(path, variable, extent, resolution, x1, y1, x2, y2)
+                quality = remap(path, qvariable, extent, resolution, x1, y1, x2, y2)
+                # Read the data returned by the function 
+                data = grid.ReadAsArray()
+                data2 = quality.ReadAsArray()
+                # Convert from int16 to uint16
+                data = data.astype(np.float64)
+                data[data == max(data[0])] = np.nan
+                data[data == min(data[0])] = np.nan
+                data2 = data2.astype(np.float64)
+                data2[data2 == max(data2[0])] = np.nan
+                data2[data2 == min(data2[0])] = np.nan
+                # Usar um backend não iterativo para que a imagem não fique aparecendo
+                # durante o processamento
+                plt.switch_backend('Agg')
+                # Define o tamanho da figura salva
+                plt.figure(figsize=(2000/float(DPI), 2000/float(DPI)), frameon=True, dpi=DPI)
+                # Plota os dados =======================================================================================
+                # Cria a referência basemap para a projeção retangular
+                bmap = Basemap(llcrnrlon=extent[0], llcrnrlat=extent[1], urcrnrlon=extent[2], urcrnrlat=extent[3], epsg=4326)
                     
-                    # Desenha os paralelos e meridianos
-                    bmap.drawparallels(np.arange(-90.0, 90.0, 2.5), linewidth=0.3, dashes=[4, 4], color='white', labels=[False,False,False,False], fmt='%g', labelstyle="+/-", xoffset=-0.80, yoffset=-1.00, size=7)
-                    bmap.drawmeridians(np.arange(0.0, 360.0, 2.5), linewidth=0.3, dashes=[4, 4], color='white', labels=[False,False,False,False], fmt='%g', labelstyle="+/-", xoffset=-0.80, yoffset=-1.00, size=7)
-                    # Aqui, vamos utilizar um arquivo de esquemas de cores.
-                    # Tais arquivos podem ser encontrados no link: http://soliton.vm.bytemark.co.uk/pub/cpt-city/
-                    # Este arquivo é baixado do site de exemplos, e é o arquivo específico para temperaturas.
-                    cpt = loadCPT('Colortables\\temperature.cpt')
-                    # Faz a interpolação linear com o arquivo CPT
-                    cpt_convert = LinearSegmentedColormap('cpt', cpt)
-                    # Plota o canal do GOES-16 com as cores convertidas do CPT
-                    # Configuração para o esquema de cores Square Root Visible Enhancement
-                    bmap.imshow(data, origin='upper', cmap=cpt_convert, vmin=0, vmax=5)
-                    # Insere a barra de cores embaixo
-                    cb = bmap.colorbar(location='bottom', size = '2%', pad = '-4%', ticks=[0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0])
+                # Desenha os países e os estados brasileiros
+                bmap.readshapefile('Shapefiles\\BRA_adm1','BRA_adm1',linewidth=0.50,color='cyan')
+                bmap.readshapefile('Shapefiles\\ne_10m_admin_0_countries','ne_10m_admin_0_countries',linewidth=0.50,color='cyan')
+                
+                # Desenha os paralelos e meridianos
+                bmap.drawparallels(np.arange(-90.0, 90.0, 2.5), linewidth=0.3, dashes=[4, 4], color='white', labels=[False,False,False,False], fmt='%g', labelstyle="+/-", xoffset=-0.80, yoffset=-1.00, size=7)
+                bmap.drawmeridians(np.arange(0.0, 360.0, 2.5), linewidth=0.3, dashes=[4, 4], color='white', labels=[False,False,False,False], fmt='%g', labelstyle="+/-", xoffset=-0.80, yoffset=-1.00, size=7)
+                # Aqui, vamos utilizar um arquivo de esquemas de cores.
+                # Tais arquivos podem ser encontrados no link: http://soliton.vm.bytemark.co.uk/pub/cpt-city/
+                # Este arquivo é baixado do site de exemplos, e é o arquivo específico para temperaturas.
+                cpt = loadCPT('Colortables\\temperature.cpt')
+                # Faz a interpolação linear com o arquivo CPT
+                cpt_convert = LinearSegmentedColormap('cpt', cpt)
+                # Plota o canal do GOES-16 com as cores convertidas do CPT
+                # Configuração para o esquema de cores Square Root Visible Enhancement
+                maxval = round(np.nanmax(data), 2)
+                minval = round(np.nanmin(data), 2)
+                bmap.imshow(data, origin='upper', cmap=cpt_convert, vmin=minval, vmax=maxval)
+                # Insere a barra de cores embaixo
+                if not np.isnan(maxval) and not np.isnan(minval):
+                    step = round((maxval-minval)/10, 2)
+                    bar_steps = np.arange(minval, maxval, step)
+                    cb = bmap.colorbar(location='bottom', size = '2%', pad = '-4%', ticks=bar_steps)
                     cb.outline.set_visible(False) # Remove the colorbar outline
                     cb.ax.tick_params(width = 0) # Remove the colorbar ticks
                     cb.ax.xaxis.set_tick_params(pad=-12.5) # Put the colobar labels inside the colorbar
                     cb.ax.tick_params(axis='x', colors='yellow', labelsize=8) # Change the color and size of the colorbar labels
                     # Passo para o exercício 3
-                    cb.ax.set_xticklabels(['0.2', '0.4', '0.6', '0.8', '1.0', '1.2', '1.4', '1.6', '1.8', '2.0', '2.2', '2.4', '2.6', '2.8', '3.0', '3.2', '3.4', '3.6', '3.8', '4.0', '4.2', '4.4', '4.6', '4.8', '5.0'])
-                    # Add a black rectangle in the bottom to insert the image description
-                    lon_difference = (extent[2] - extent[0]) # Max Lon - Min Lon
-                    currentAxis = plt.gca()
-                    currentAxis.add_patch(Rectangle((extent[0], extent[1]), lon_difference, lon_difference * 0.015, alpha=1, zorder=3, facecolor='black'))
-                    
-                    # Save the result
-                    if not os.path.exists('Output\\' + date.strftime("%Y\\%m\\%d\\")):
-                        os.makedirs('Output\\' + date.strftime("%Y\\%m\\%d\\"))
-                        print('Criado diretório Output\\' + date.strftime("%Y\\%m\\%d\\"))
-                    plt.savefig('Output\\' + date.strftime("%Y\\%m\\%d\\") + 'G16_' + nomenclature + '-' + date.strftime("%Y.%m.%d.%H.%M") + '.png', dpi=DPI, bbox_inches='tight', pad_inches=0)
-                    plt.close()
-                    print('Gerado .png: ', 'G16_' + nomenclature + '-' + date.strftime("%Y.%m.%d.%H.%M") + '.png')
-                    # Export the result to GeoTIFF ================================================
-                    # Habilita excessões para esta parte do código
-                    gdal.UseExceptions()
-                    # Get GDAL driver GeoTiff
-                    driver = gdal.GetDriverByName('GTiff')
-                    # Get dimensions
-                    nlines = data.shape[0]
-                    ncols = data.shape[1]
-                    data_type = gdal.GDT_Float32 # gdal.GDT_Int16 # gdal.GDT_Float32
-                    # Create grid
-                    #options = ['COMPRESS=JPEG', 'JPEG_QUALITY=80', 'TILED=YES']
-                    grid = driver.Create('grid', ncols, nlines, 2, data_type)#, options)
-                    # Write data for each bands
-                    grid.GetRasterBand(1).WriteArray(data)
-                    grid.GetRasterBand(2).WriteArray(data2)
-                    # Lat/Lon WSG84 Spatial Reference System
-                    srs = osr.SpatialReference()
-                    srs.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
-                    # Setup projection and geo-transform
-                    grid.SetProjection(srs.ExportToWkt())
-                    grid.SetGeoTransform(getGeoTransform(extent, nlines, ncols))
-                    # Save the file
-                    #print('G16_' + product + '_' + variable + '_' + date + '.tif')
-                    #driver.CreateCopy('G16_' + product + '_' + variable + '_' + date + '.tif', grid, 0)
-                    print('GeoTIFF gerado: ', 'Output\\' + date.strftime("%Y\\%m\\%d\\") + 'G16_' + nomenclature + '_' + date.strftime("%Y-%m-%d-%H-%M") + '.tif')
-                    driver.CreateCopy('Output\\' + date.strftime("%Y\\%m\\%d\\") + 'G16_' + nomenclature + '-' + date.strftime("%Y.%m.%d.%H.%M") + '.tif', grid, 0)	
-                    # Close the file
-                    driver = None
-                    grid = None
-                    # Delete the grid
-                    os.remove('grid')
-                    # Delete the aux file
-                    os.remove(path + '.aux.xml')
-                    # Generating the SST and LST daily accumulate
-                    if product == "LSTF":
-                        start_day = date[0:8]
-                        gdal.BuildVRT('G16_' + nomenclature + '_' + date + '.vrt', sorted(glob.glob('G16_' + nomenclature + '_' + start_day + '*.tif'), key=os.path.getmtime), srcNodata = 0)# , reverse=True), srcNodata = 0)
-                        gdal.Translate('G16_' + nomenclature[0:4] + 'DA_' + date + '.tif', 'G16_' + nomenclature + '_' + date + '.vrt')       
-                        print('Generated GeoTIFF: ','G16_' + nomenclature[0:4] + 'DA_' + date + '.tif')  
-                        os.remove('G16_' + nomenclature + '_' + date + '.vrt')
-                    elif product == "TPWF":
-                        start_day = date[0:8]
-                        gdal.BuildVRT('G16_' + nomenclature + '_' + date + '.vrt', sorted(glob.glob('G16_' + nomenclature + '_' + start_day + '*.tif'), key=os.path.getmtime), srcNodata = np.nan)# , reverse=True), srcNodata = 0)
-                        gdal.Translate('G16_' + nomenclature[0:4] + 'DA_' + date + '.tif', 'G16_' + nomenclature + '_' + date + '.vrt')       
-                        print('Generated GeoTIFF: ','G16_' + nomenclature[0:4] + 'DA_' + date + '.tif')  
-                        os.remove('G16_' + nomenclature + '_' + date + '.vrt')
-                        os.remove('G16_' + nomenclature[0:4] + 'DA_' + date + '.tif.aux.xml')
-                    elif product == "SSTF":
-                        start_day = date[0:6]
-                        gdal.BuildVRT('G16_' + nomenclature + '_' + date + '.vrt', sorted(glob.glob('G16_' + nomenclature + '_' + start_day + '*.tif'), key=os.path.getmtime), srcNodata = 0)# , reverse=True), srcNodata = 0)
-                        gdal.Translate('G16_' + nomenclature[0:4] + 'MA_' + date + '.tif', 'G16_' + nomenclature + '_' + date + '.vrt')       
-                        print('Generated GeoTIFF: ','G16_' + nomenclature[0:4] + 'MA_' + date + '.tif')  
-                        os.remove('G16_' + nomenclature + '_' + date + '.vrt')
-                    gdal.DontUseExceptions()
+                    cb.ax.set_xticklabels(bar_steps)
+                # Add a black rectangle in the bottom to insert the image description
+                lon_difference = (extent[2] - extent[0]) # Max Lon - Min Lon
+                currentAxis = plt.gca()
+                currentAxis.add_patch(Rectangle((extent[0], extent[1]), lon_difference, lon_difference * 0.015, alpha=1, zorder=3, facecolor='black'))
+                
+                # Save the result
+                if not os.path.exists('Output\\' + nomenclature + '\\' + date.strftime("%Y\\%m\\%d\\")):
+                    os.makedirs('Output\\' + nomenclature + '\\' + date.strftime("%Y\\%m\\%d\\"))
+                    print('Criado diretório Output\\' + nomenclature + '\\' + date.strftime("%Y\\%m\\%d\\"))
+                plt.savefig('Output\\' + nomenclature + '\\' + date.strftime("%Y\\%m\\%d\\") + 'G16_' + nomenclature + '-' + date.strftime("%Y.%m.%d.%H.%M") + '.png', dpi=DPI, bbox_inches='tight', pad_inches=0)
+                plt.close()
+                print('Gerado .png: ', 'G16_' + nomenclature + '-' + date.strftime("%Y.%m.%d.%H.%M") + '.png')
+                # Export the result to GeoTIFF ================================================
+                # Habilita excessões para esta parte do código
+                gdal.UseExceptions()
+                # Get GDAL driver GeoTiff
+                driver = gdal.GetDriverByName('GTiff')
+                # Get dimensions
+                nlines = data.shape[0]
+                ncols = data.shape[1]
+                data_type = gdal.GDT_Float32 # gdal.GDT_Int16 # gdal.GDT_Float32
+                # Create grid
+                #options = ['COMPRESS=JPEG', 'JPEG_QUALITY=80', 'TILED=YES']
+                grid = driver.Create('grid', ncols, nlines, 2, data_type)#, options)
+                # Write data for each bands
+                grid.GetRasterBand(1).WriteArray(data)
+                grid.GetRasterBand(2).WriteArray(data2)
+                # Lat/Lon WSG84 Spatial Reference System
+                srs = osr.SpatialReference()
+                srs.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+                # Setup projection and geo-transform
+                grid.SetProjection(srs.ExportToWkt())
+                grid.SetGeoTransform(getGeoTransform(extent, nlines, ncols))
+                # Save the file
+                #print('G16_' + product + '_' + variable + '_' + date + '.tif')
+                #driver.CreateCopy('G16_' + product + '_' + variable + '_' + date + '.tif', grid, 0)
+                print('GeoTIFF gerado: ', 'Output\\' + nomenclature + '\\' + date.strftime("%Y\\%m\\%d\\") + 'G16_' + nomenclature + '_' + date.strftime("%Y-%m-%d-%H-%M") + '.tif')
+                driver.CreateCopy('Output\\' + nomenclature + '\\' + date.strftime("%Y\\%m\\%d\\") + 'G16_' + nomenclature + '-' + date.strftime("%Y.%m.%d.%H.%M") + '.tif', grid, 0)	
+                # Close the file
+                driver = None
+                grid = None
+                # Delete the grid
+                os.remove('grid')
+                # Delete the aux file
+                os.remove(path + '.aux.xml')
+                # Generating the SST and LST daily accumulate
+                if product == "LSTF":
+                    start_day = date[0:8]
+                    gdal.BuildVRT('G16_' + nomenclature + '_' + date + '.vrt', sorted(glob.glob('G16_' + nomenclature + '_' + start_day + '*.tif'), key=os.path.getmtime), srcNodata = 0)# , reverse=True), srcNodata = 0)
+                    gdal.Translate('G16_' + nomenclature[0:4] + 'DA_' + date + '.tif', 'G16_' + nomenclature + '_' + date + '.vrt')       
+                    print('Generated GeoTIFF: ','G16_' + nomenclature[0:4] + 'DA_' + date + '.tif')  
+                    os.remove('G16_' + nomenclature + '_' + date + '.vrt')
+                elif product == "TPWF":
+                    start_day = date[0:8]
+                    gdal.BuildVRT('G16_' + nomenclature + '_' + date + '.vrt', sorted(glob.glob('G16_' + nomenclature + '_' + start_day + '*.tif'), key=os.path.getmtime), srcNodata = np.nan)# , reverse=True), srcNodata = 0)
+                    gdal.Translate('G16_' + nomenclature[0:4] + 'DA_' + date + '.tif', 'G16_' + nomenclature + '_' + date + '.vrt')       
+                    print('Generated GeoTIFF: ','G16_' + nomenclature[0:4] + 'DA_' + date + '.tif')  
+                    os.remove('G16_' + nomenclature + '_' + date + '.vrt')
+                    os.remove('G16_' + nomenclature[0:4] + 'DA_' + date + '.tif.aux.xml')
+                elif product == "SSTF":
+                    start_day = date[0:6]
+                    gdal.BuildVRT('G16_' + nomenclature + '_' + date + '.vrt', sorted(glob.glob('G16_' + nomenclature + '_' + start_day + '*.tif'), key=os.path.getmtime), srcNodata = 0)# , reverse=True), srcNodata = 0)
+                    gdal.Translate('G16_' + nomenclature[0:4] + 'MA_' + date + '.tif', 'G16_' + nomenclature + '_' + date + '.vrt')       
+                    print('Generated GeoTIFF: ','G16_' + nomenclature[0:4] + 'MA_' + date + '.tif')  
+                    os.remove('G16_' + nomenclature + '_' + date + '.vrt')
+                gdal.DontUseExceptions()
         print("Conversão Concluída!")
 
 app = QtWidgets.QApplication([])
